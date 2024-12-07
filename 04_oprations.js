30c5a0f5d2da539bedaaa294221a3e31eca3b22f
MudassarMukhtar <97121751+MudassarMukhtar@users.noreply.github.com>
2024-12-07 23:39:00 +0500

    javascript

---
+++ 04_oprations.js
@@ -0,0 +1,35 @@
+let value=3;
+let negvalue=-value;
+//console.log(negvalue)
+
+
+//+,-,*,/,%,**
+// 2+3
+// 2-3
+// 2*3
+// 2/3
+// 2**3// 2 to powers 3
+// 2%3
+
+let str_1="hello"
+let str_2="ali";
+
+let str3=str_2+str_1;
+console.log(str3);
+
+console.log(2+"2");//22
+console.log("2"+2);//22
+console.log("1"+2+2);//122
+console.log(1+2+"2");//32
+
+
+// prefix 
+// increment first then assign
+let num=1;
+   ++num
+console.log(num);
+// postfix
+// first assign then increment
+let num_2=1;
+   num_2++
+console.log(num_2);
---
+++ 05_comparsion.js
@@ -0,0 +1,16 @@
+console.log(2>10)
+console.log(3<4)
+console.log(2<=3)
+console.log(2==1)
+console.log(2!=1)
+
+//AVoid these type of conversion
+console.log(null==0)//false
+console.log(null>=0)//true
+console.log(null>0)//false
+
+console.log(undefined==0)//false
+console.log(undefined>=0)//false
+console.log(undefined>0)//false
+
+console.log("2"===2)//check its type and equality
